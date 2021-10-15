12024b38a094ff32f2241603d074d3460f71a63b
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-15 11:24:24 +0100

    Add test to view homepage title

--- spec/features/viewing_homepage_spec.rb
+++ spec/features/viewing_homepage_spec.rb
@@ -1,9 +1,13 @@
 feature "Viewing home page" do
   scenario "Viewing welcome message" do
     add_row_to_test_database
-
     visit "/"
 
     expect(page).to have_content "Welcome to Chitter!"
+  end
+
+  scenario "Viewing page title" do
+    visit "/"
+    expect(page).to have_title "Chitter"
   end
 end
