ab1238b49c00e7486ca850050cf3284390bc48f4
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-15 11:13:29 +0100

    Add test to view homepage

--- spec/features/test_page_spec.rb
+++ spec/features/test_page_spec.rb
@@ -1,6 +1,14 @@
-feature 'Viewing test page' do
-  scenario 'visiting the test page' do
-    visit('/test')
+feature "Viewing test page" do
+  scenario "visiting the test page" do
+    visit("/test")
     expect(page).to have_content "Test page"
+  end
+
+  scenario "Viewing homepage" do
+    add_row_to_test_database
+
+    visit "/"
+
+    expect(page).to have_content "Welcome to Chitter!"
   end
 end
