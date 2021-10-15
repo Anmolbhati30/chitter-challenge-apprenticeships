90e57167039ac8c5145f5b0dc77b60943417feb7
Anmol <bhatianmol566@yahoo.co.uk>
2021-10-15 11:13:40 +0100

    Pass test to view homepage

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
-require 'sinatra/base'
+require "sinatra/base"
 
 class Chitter < Sinatra::Base
-  get '/test' do
-    'Test page'
+  get "/test" do
+    "Test page"
+  end
+
+  get "/" do
+    "Welcome to Chitter!"
   end
 
   run! if app_file == $0
